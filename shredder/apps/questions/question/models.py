c7199fe999c8e10786d5e9a1058870638db074c1
zhou weitao <weizhou@redhat.com>
2012-12-13 11:02:20 +0800

    ADD: Added type for model question and add model choice to save choice&answer

--- shredder/apps/questions/question/models.py
+++ shredder/apps/questions/question/models.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # Added by Chaobin Tang <chaobin.py@gmail.com>, Weitao Zhou <zhouwtlord@gmail.com>
 
-from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.db import models
 from django.utils.text import truncate_words
 
 from apps.common.models import Base
@@ -11,7 +11,7 @@ import apps.questions.settings as question_settings
 
 
 __all__ = [
-    'Tag', 'Question',
+    'Tag', 'Question', 'Candidate',
 ]
 
 
@@ -42,6 +42,12 @@ class Question(Base):
     )
     STATUS = dict(STATUS_NODE)
 
+    QUESTION_TYPE = (
+        (1, u'Subjective'),
+        (2, u'Objective'),
+    )
+    TYPE = dict(QUESTION_TYPE)
+
     description = models.CharField(max_length=question_settings.QUESTION_MAX_LENGTH)
     difficulty = models.IntegerField(
         choices=QUESTION_DIFFICULTY,
@@ -49,6 +55,10 @@ class Question(Base):
     )
     status = models.IntegerField(
         choices=STATUS_NODE,
+        default=1
+    )
+    type = models.IntegerField(
+        choices=QUESTION_TYPE,
         default=1
     )
     estimated_time = models.IntegerField(u'Number of minutes needed to give the answer', default=60)
@@ -67,3 +77,31 @@ class Question(Base):
         text = [tag.name for tag in self.tags.all()]
         return u','.join(text)
     textual_tags = property(_get_textual_tags)
+
+
+class ChoiceManager(models.Manager):
+    
+    def answer(self, keyword):
+        """Return a list containing the candidates which are/is the answer of question."""
+
+        return self.filter(question=keyword).filter(is_answer=True)
+
+
+class Choice(Base):
+    """Because we can look the subjective question as an objective question
+
+    which only has one choice, the subjective question is generalized as an
+    objective question. The field is_answer tags if the choice is answer."""
+
+    description = models.CharField(
+        verbose_name="Choice-Answer",
+        max_length=question_settings.QUESTION_MAX_LENGTH,
+    )
+    question = models.ForeignKey(Question)
+    is_answer = models.BooleanField(default=False)
+    
+    objects = ChoiceManager()
+
+    def __str__(self):
+        return u'Choice - %s' % truncate_words(self.description, 5)
+    __unicode__ = __str__
